2b661f2f4a5fbe8dce14fe1a89533563607f81ba
IzumiShaka-desu <shakaaji29@gmail.com>
2023-10-05 13:49:52 +0700

    feat: setup commons

---
+++ lib/common/constants.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+abstract class Constants {
+  static const String baseUrl = 'https://pokeapi.co/api/v2';
+  static const String baseUrlSvgImages =
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/';
+  static const String baseUrlPngImages =
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/';
+  static const String baseUrlGifImages =
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/';
+}
